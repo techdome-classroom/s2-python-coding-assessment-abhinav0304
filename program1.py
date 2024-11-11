e34fc964b7fafbfacc921cb21151fe1d5d59dadf
Avinash Kole <avinashkole@Avinashs-MacBook-Air.local>
2024-11-11 15:56:48 +0530

    Changes

--- program1.py
+++ program1.py
@@ -4,7 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+        matching_bracket = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in matching_bracket:
+                if stack and stack[-1] == matching_bracket[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
 
 
 
